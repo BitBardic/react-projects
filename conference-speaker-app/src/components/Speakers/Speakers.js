c8aff272cd85404ec8547c3cb0742942db279db1
Nasir Iqbal <nasirpucit@hotmail.com>
2021-03-17 16:08:21 +0000

    introducing render props as a refactoring strategy

--- conference-speaker-app/src/components/Speakers/Speakers.js
+++ conference-speaker-app/src/components/Speakers/Speakers.js
@@ -1,27 +1,20 @@
 import React from 'react';
+import SpeakersRenderProps from './SpeakersRenderProps';
 
-const Speakers = ({speakers}) => {
+const Speakers = () => {  
     return(
-        <div>
-            {speakers.map(({imageSrc, name}) => {
-                return  <img src={`images/${imageSrc}.png`} alt={name} key={imageSrc}></img>;
-            })}
-        </div> 
+        <SpeakersRenderProps>
+            {({speakers}) => {
+                return (
+                    <div>
+                        {speakers.map(({ imageSrc, name }) => {
+                            return  <img src={`images/${imageSrc}.png`} alt={name} key={imageSrc}></img>;
+                        })}
+                    </div> 
+                );
+            }}
+        </SpeakersRenderProps>
     );
 };
 
-const EnhancedSpeakerComponent = withData(Speakers);
-
-function withData(Component) {
-    const speakers = [
-        { imageSrc: 'speaker-component-1124', name: 'Douglas Crockford' },
-        { imageSrc: 'speaker-component-1530', name: 'Tamara Baker' },
-        { imageSrc: 'speaker-component-10803', name: 'Eugene Chuvyrov'}
-    ];
-    return function(){
-      return <Component speakers={speakers}></Component>;
-    };
-  };
-
-
-export default EnhancedSpeakerComponent;
+export default Speakers;
