d7cd67bea157b47a1672c47a2179773641cfba77
Nasir Iqbal <nasirpucit@hotmail.com>
2021-03-18 11:49:02 +0000

    reverted back to before applying patterns

--- conference-speaker-app/pages/speakers.js
+++ conference-speaker-app/pages/speakers.js
@@ -5,24 +5,13 @@ import Speakers from "../src/components/Speakers/Speakers";
 import Footer from "../src/components/Footer/Footer";
 import React from "react";
 
-import SpeakerContext from '../src/components/Speakers/SpeakerContext';
-
 function Page() {
-
-    const speakers = [
-        { imageSrc: 'speaker-component-1124', name: 'Douglas Crockford' },
-        { imageSrc: 'speaker-component-1530', name: 'Tamara Baker' },
-        { imageSrc: 'speaker-component-10803', name: 'Eugene Chuvyrov'}
-    ];
-
     return (
       <div>
         <Header />
         <Menu />
-        <SpeakerContext.Provider value={speakers}>
-            <SpeakerSearchBar />
-            <Speakers />
-        </SpeakerContext.Provider>
+        <SpeakerSearchBar />
+        <Speakers />
         <Footer />
       </div>
     );
